18a2542cf4e04acaf92a511d11bfa4bee0b15f69
mladen.hrkic <mladenhrkic@gmail.com>
2024-12-31 20:08:11 +0100

    add project

---
+++ ConsoleSignalR/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+const string apiKey = "68f3a6db-36b8-411f-99c9-7f4a69a524ea";  
+const string hubUrl = "https://localhost:7190/chat-hub";  
+
+var connection = new HubConnectionBuilder()
+    .WithUrl(hubUrl, options =>
+    {
+        options.Headers.Add("ApiKey", apiKey);
+    })
+    .Build();
+
+connection.On<string>("ReceiveMessage", (message) =>
+{
+    Console.WriteLine($"Message received: {message}");
+});
+
+try
+{
+    await connection.StartAsync();
+    Console.WriteLine("Connected...");
+
+    await connection.InvokeAsync("JoinGroup", "ConsoleAppGroup");
+
+    Console.WriteLine("Press Enter to exit.");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error connecting to the hub: {ex.Message}");
+}
+
+Console.ReadLine();
